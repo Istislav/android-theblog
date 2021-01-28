0757372d3fe6f09b4fd7f9416aaad48d9cac96da
CyberUnit <cyberunit@gmail.com>
2021-01-28 21:18:14 +0300

    login and register button, login method is working

--- app/src/main/java/ru/istislav/theblog/MainActivity.java
+++ app/src/main/java/ru/istislav/theblog/MainActivity.java
@@ -1,14 +1,98 @@
 package ru.istislav.theblog;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mUser;
+    private Button loginButton;
+    private Button createActButton;
+    private EditText emailField;
+    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton = (Button) findViewById(R.id.loginBtn);
+        createActButton = (Button) findViewById(R.id.createAccBtn);
+        emailField = (EditText) findViewById(R.id.loginEmailEd);
+        passwordField = (EditText) findViewById(R.id.loginPasswordEd);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mUser = firebaseAuth.getCurrentUser();
+
+                if (mUser != null) {
+                    Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "NOT Signed in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(emailField.getText().toString())
+                        && !TextUtils.isEmpty(passwordField.getText().toString())) {
+                    String email = emailField.getText().toString();
+                    String pwd = passwordField.getText().toString();
+
+                    login(email, pwd);
+                } else {
+
+                }
+            }
+        });
+
+    }
+
+    private void login(String email, String pwd) {
+        mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void register(String email, String pwd) {
+        mAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 }
