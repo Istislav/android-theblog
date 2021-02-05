946f945fed772f3a55d247e0eac71ae1daaaed9a
CyberUnit <cyberunit@gmail.com>
2021-02-05 21:48:29 +0300

    adding title and description

--- app/src/main/java/ru/istislav/theblog/Activities/AddPostActivity.java
+++ app/src/main/java/ru/istislav/theblog/Activities/AddPostActivity.java
@@ -2,15 +2,83 @@ package ru.istislav.theblog.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import ru.istislav.theblog.Model.Blog;
 import ru.istislav.theblog.R;
 
 public class AddPostActivity extends AppCompatActivity {
+
+    private ImageButton mPostImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSaveButton;
+
+    private DatabaseReference mPostDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+
+        mProgress = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        mPostDatabase = FirebaseDatabase.getInstance().getReference().child("MBloc");
+
+        mPostImage = (ImageButton) findViewById(R.id.postImageButton);
+        mPostTitle = (EditText) findViewById(R.id.editPostTitle);
+        mPostDesc = (EditText) findViewById(R.id.editPostDescription);
+        mSaveButton = (Button) findViewById(R.id.savePostButton);
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPosting();
+            }
+
+        });
+    }
+
+    private void startPosting() {
+        mProgress.setMessage("Posting to blog...");
+        mProgress.show();
+
+        String titleVal = mPostTitle.getText().toString().trim();
+        String descVal = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(titleVal) && !TextUtils.isEmpty(descVal)) {
+            Blog blog = new Blog(titleVal, descVal, "imageVal",
+                    "timestamp", "userid");
+            mPostDatabase.setValue(blog).addOnSuccessListener(new OnSuccessListener<Void>() {
+                      @Override
+                      public void onSuccess(Void aVoid) {
+                          Toast.makeText(getApplicationContext(), "Item added",
+                                            Toast.LENGTH_SHORT).show();
+                          mProgress.dismiss();
+                      }
+                  }
+            );
+        }
     }
 }
