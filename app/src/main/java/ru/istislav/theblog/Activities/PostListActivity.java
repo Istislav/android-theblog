6db7ac72fe21605f1dd9c2568af422adf382cd04
CyberUnit <cyberunit@gmail.com>
2021-02-05 21:22:29 +0300

    actions in menu in the post list

--- app/src/main/java/ru/istislav/theblog/Activities/PostListActivity.java
+++ app/src/main/java/ru/istislav/theblog/Activities/PostListActivity.java
@@ -3,18 +3,35 @@ package ru.istislav.theblog.Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import ru.istislav.theblog.R;
 
 public class PostListActivity extends AppCompatActivity {
 
+    private DatabaseReference mDatabaseReference;
+    private FirebaseDatabase mDatabase;
+    private FirebaseUser mUser;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_list);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference().child("MBlog");
+        mDatabaseReference.keepSynced(true);
     }
 
     @Override
@@ -25,6 +42,21 @@ public class PostListActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                if (mUser != null && mAuth != null) {
+                    startActivity(new Intent(PostListActivity.this, AddPostActivity.class));
+                    finish();
+                }
+                break;
+            case R.id.action_signout:
+                if (mUser != null && mAuth != null) {
+                    mAuth.signOut();
+                    startActivity(new Intent(PostListActivity.this, MainActivity.class));
+                }
+                break;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 }
